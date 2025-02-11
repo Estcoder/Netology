a7e7cdc6368565a92a21a0cc889e4309897c53b5
Estcoder <estcoder@gmail.com>
2025-02-11 23:47:50 +0200

    Add files via upload

---
+++ HW_FIRST.java
@@ -0,0 +1,19 @@
+public class HW_FIRST {
+    public static void main(String[] args) {
+
+        String name = "Dmitrii Sokolov";
+        System.out.println(name);
+
+        System.out.println();
+
+        int income = 44000;
+        int spending= 30000;
+        int sum = (income - spending);
+        System.out.println ("Итого (руб): ");
+        System.out.println(sum);
+
+
+
+
+    }
+}
